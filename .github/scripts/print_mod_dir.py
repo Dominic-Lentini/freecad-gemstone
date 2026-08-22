45338915f59fe139072ec841e97d7425ac04b9bc
Dominic Lentini <58958053+Dominic-Lentini@users.noreply.github.com>
2026-08-22 13:09:12 -0400

    Change output label from LAPIDARY_MOD_DIR to GEMSTONE_MOD_DIR

--- .github/scripts/print_mod_dir.py
+++ .github/scripts/print_mod_dir.py
@@ -7,6 +7,6 @@ import FreeCAD  # noqa: F401  (must run inside FreeCADCmd)
 
 import sys
 
-print("LAPIDARY_MOD_DIR=" + os.path.join(FreeCAD.getUserAppDataDir(), "Mod"))
+print("GEMSTONE_MOD_DIR=" + os.path.join(FreeCAD.getUserAppDataDir(), "Mod"))
 sys.stdout.flush()
 os._exit(0)
